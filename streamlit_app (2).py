a8981030c826e90ee468515f5abf5397206ea909
gametjantan <163863767+gametjantan@users.noreply.github.com>
2025-09-15 15:56:20 +0700

    Update streamlit_app (2).py

--- streamlit_app (2).py
+++ streamlit_app (2).py
@@ -57,6 +57,9 @@ st.markdown("""
     .landing-description {
         font-size: 1.1rem;
         margin-bottom: 3rem;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
         max-width: 800px;
         line-height: 1.6;
     }
